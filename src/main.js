4182b917fc5c3e272eb92f3b18a8dc93be17c1b6
COOL_CAL <grin0330@gmail.com>
2022-07-15 17:20:11 +0900

    카톡 소셜 로그인

--- src/main.js
+++ src/main.js
@@ -1,6 +1,14 @@
+import 'axios'
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import mixins from './mixins'
 
-createApp(App).use(store).use(router).mount('#app')
+createApp(App)
+.mixin(mixins)
+.use(store)
+.use(router)
+.mount('#app');
+
+window.Kakao.init('4f117a4b68dbaa7046253d5074e266ca');
