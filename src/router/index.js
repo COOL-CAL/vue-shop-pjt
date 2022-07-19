f1abe11e69316d8f5615d4d7c7575536c7bb5be3
COOL_CAL <grin0330@gmail.com>
2022-07-19 18:06:32 +0900

    제품 디테일

--- src/router/index.js
+++ src/router/index.js
@@ -6,6 +6,17 @@ import ProductCreate from '../views/ProductCreate';
 import ProductUpdate from '../views/ProductUpdate';
 import SalesList from '../views/SalesList';
 import ImageInsert from '../views/ImageInsert';
+import store from '@/store';
+import swal from 'sweetalert2';
+
+//네비게이션 가드
+const requireAuth = () => (to, from, next) => { //to: 가고자하는 위치, from: 현재 위치, next: 콜백함수
+  if(store.state.user.iuser === undefined) {
+    swal.fire('로그인이 필요합니다.', '', 'warning');
+    return;
+  }
+  return next();
+}
 
 const routes = [  
   {
@@ -21,7 +32,8 @@ const routes = [
   {
     path: '/create',
     name: 'ProductCreate',
-    component: ProductCreate
+    component: ProductCreate,
+    beforeEnter: requireAuth() //여기서 위치 체크 하기 때문에 to, from 안 써도 됨
   },
   {
     path: '/update',
@@ -36,7 +48,8 @@ const routes = [
   {
     path: '/image_insert',
     name: 'ImageInsert',    
-    component: ImageInsert 
+    component: ImageInsert,
+    beforeEnter: requireAuth()
   }
 ];
 
